6cc2a8c36d64dddfcab612883cbf2ca31d97f141
dariuszbensch <dariuszbensch@users.noreply.github.com>
2019-02-14 04:29:34 +0000

    Added code

--- CSharp_ConsoleApp_BubbleSort/Program.cs
+++ CSharp_ConsoleApp_BubbleSort/Program.cs
@@ -1,15 +1,59 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace CSharp_ConsoleApp_BubbleSort
+
+
+namespace ConsoleApp_BubbleSort1
 {
     class Program
     {
         static void Main(string[] args)
         {
+            int[] tab = new int[] { 1, 4, 2, 4, 5, 7, 4, 3, 6, 3, 0, 0 };
+            Sorting a = new Sorting();
+
+            foreach (int n in a.fromLowest(tab)) Console.WriteLine(n);
+            foreach (int n in a.fromHighest(tab)) Console.WriteLine(n);
+
+            Console.ReadKey();
+        }
+    }
+
+
+
+    class Sorting
+    {
+        int temporaryValueKeeper = 0;
+
+        public int[] fromLowest(int[] tab)
+        {
+            for (int i = 0; i < tab.GetLength(0) - 1; i++)
+            {
+                if (tab[i] > tab[i + 1])
+                {
+                    temporaryValueKeeper = tab[i];
+                    tab[i] = tab[i + 1];
+                    tab[i + 1] = temporaryValueKeeper;
+                    i = -1;
+                }
+            }
+            return tab;
+        }
+
+
+
+        public int[] fromHighest(int[] tab)
+        {
+            for (int i = 0; i < tab.GetLength(0) - 1; i++)
+            {
+                if (tab[i] < tab[i + 1])
+                {
+                    temporaryValueKeeper = tab[i];
+                    tab[i] = tab[i + 1];
+                    tab[i + 1] = temporaryValueKeeper;
+                    i = -1;
+                }
+            }
+            return tab;
         }
     }
 }
